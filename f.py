697465cce7c3e7c2f01465851e5400e0244f82ac
BoppreH <boppreh@gmail.com>
2016-06-19 15:20:11 -0300

    Add grep

--- f.py
+++ f.py
@@ -28,8 +28,10 @@ while args and not os.path.exists(args[0]):
 assert len(args) == 0, 'Unexpected existing paths at the end of command: ' + ' '.join(args)
 
 if not inputs and not existing and not new:
-	os.system('ls -lah')
+	subprocess.call(['ls', '-lah'])
 elif not inputs and not new and len(existing) == 1 and os.path.isdir(existing[0]):
 	print("cd functionality not working at the moment.")
+elif len(inputs) == 1 and existing and not new:
+	subprocess.call(['grep', '-r'] + inputs + existing)
 else:
 	print("Unexpected commands. Don't know what to do.")
