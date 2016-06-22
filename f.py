b321543278c2c52c16eb5227aae6c9597fdf7f37
BoppreH <boppreh@gmail.com>
2016-06-22 15:50:16 -0300

    Put code into function

--- f.py
+++ f.py
@@ -25,55 +25,61 @@ def find_replace(pattern, replacement, paths):
 		else:
 			call_on_file(['vim', '-c', command, '-c', 'wq'], path)
 
-args = list(sys.argv[1:])
+image_extensions = ['.jpg', '.jpeg', '.gif', '.png']
+doc_extensions = ['.txt', '.md', '.rst', '.html', '.pdf', '.docx', '.odt']
 
-inputs = []
-existing = []
-new = []
+def invoke(args):
+	inputs = []
+	existing = []
+	new = []
 
-while args and not os.path.exists(args[0]):
-	inputs.append(args.pop(0))
+	while args and not os.path.exists(args[0]):
+		inputs.append(args.pop(0))
 
-while args and os.path.exists(args[0]):
-	existing.append(args.pop(0))
+	while args and os.path.exists(args[0]):
+		existing.append(args.pop(0))
 
-while args and not os.path.exists(args[0]):
-	new.append(args.pop(0))
+	while args and not os.path.exists(args[0]):
+		new.append(args.pop(0))
 
-assert len(args) == 0, 'Unexpected existing paths at the end of command: ' + ' '.join(args)
+	assert len(args) == 0, 'Unexpected existing paths at the end of command: ' + ' '.join(args)
 
-if not inputs and not existing and not new:
-	subprocess.call(['ls', '-lah'])
-elif not inputs and not new and len(existing) == 1 and os.path.isdir(existing[0]):
-	call_on_file(['ls', '-lah', '--color', 'always'], existing[0])
-elif not inputs and not new and len(existing) == 1 and existing[0].endswith(('.zip', '.tar.gz', '.tar.bz2', '.7z', '.rar')):
-	file, = existing
-	if file.endswith('.zip'):
-		call_on_file(['zipinfo'], file)
-		if input('Extract? [y/N]')[0].lower() == 'y':
-			call_on_file(['unzip'], file)
+	if not inputs and not existing and not new:
+		subprocess.call(['ls', '-lah'])
+	elif not inputs and not new and len(existing) == 1 and os.path.isdir(existing[0]):
+		call_on_file(['ls', '-lah', '--color', 'always'], existing[0])
+	elif not inputs and not new and len(existing) == 1 and existing[0].endswith(('.zip', '.tar.gz', '.tar.bz2', '.7z', '.rar')):
+		file, = existing
+		if file.endswith('.zip'):
+			call_on_file(['zipinfo'], file)
+			if input('Extract? [y/N]')[0].lower() == 'y':
+				call_on_file(['unzip'], file)
+		else:
+			raise ValueError(file)
+	elif not inputs and not new and len(existing) == 1 and os.path.isfile(existing[0]):
+		start_file(existing[0])
+	elif len(inputs) == 1 and not new and not existing and not Path(inputs[0]).suffix:
+		try:
+			os.makedirs(inputs[0])
+		except PermissionError:
+			subprocess.call(['sudo', 'mkdir', '-p', inputs[0]])
+	elif len(new) == 1 and existing and not inputs and new[0].endswith('.zip'):
+		subprocess.call(['zip', '-r'] + new + existing)
+	elif len(inputs) == 1 and existing and not new:
+		subprocess.call(['grep', '-r'] + inputs + existing)
+	elif len(inputs) == 2 and existing and not new:
+		pattern, replacement = inputs
+		find_replace(pattern, replacement, map(Path, existing))
+	elif not inputs and ((len(new) == 1 and not Path(new[0]).suffix) or (not new and len(existing) > 1 and os.path.isdir(existing[-1]))):
+		if new:
+			os.makedirs(new[0])
+		subprocess.call(['mv'] + existing + new)
 	else:
-		raise ValueError(file)
-elif not inputs and not new and len(existing) == 1 and os.path.isfile(existing[0]):
-	start_file(existing[0])
-elif len(inputs) == 1 and not new and not existing and not Path(inputs[0]).suffix:
-	try:
-		os.makedirs(inputs[0])
-	except PermissionError:
-		subprocess.call(['sudo', 'mkdir', '-p', inputs[0]])
-elif len(new) == 1 and existing and not inputs and new[0].endswith('.zip'):
-	subprocess.call(['zip', '-r'] + new + existing)
-elif len(inputs) == 1 and existing and not new:
-	subprocess.call(['grep', '-r'] + inputs + existing)
-elif len(inputs) == 2 and existing and not new:
-	pattern, replacement = inputs
-	find_replace(pattern, replacement, map(Path, existing))
-elif not inputs and ((len(new) == 1 and not Path(new[0]).suffix) or (not new and len(existing) > 1 and os.path.isdir(existing[-1]))):
-	if new:
-		os.makedirs(new[0])
-	subprocess.call(['mv'] + existing + new)
-else:
-	print("Unexpected commands. Don't know what to do.")
-	print('inputs:', inputs)
-	print('existing:', existing)
-	print('new:', new)
+		print("Unexpected commands. Don't know what to do.")
+		print('inputs:', inputs)
+		print('existing:', existing)
+		print('new:', new)
+
+
+if __name__ == '__main__':
+	invoke(list(sys.argv[1:]))
