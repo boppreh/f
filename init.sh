9e85b34591a38caba65090a72b95e1c23823f7d2
BoppreH <boppreh@gmail.com>
2016-08-10 17:43:45 -0300

    ls after cd

--- init.sh
+++ init.sh
@@ -5,6 +5,7 @@ fdir=$(dirname "${BASH_SOURCE[0]}")
 function f() {
     if test "$#" -eq 1 && test -d "$1"; then
         cd "$1"
+        python3 "$fdir/f.py"
     elif test "$#" -eq 1 && test "$1" == "-"; then
         cd -
     else
