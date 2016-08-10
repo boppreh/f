513963756c25964cb11a373c69e97ad54c6ca988
BoppreH <boppreh@gmail.com>
2016-08-10 14:43:49 -0300

    Use 'f -' to go back one level

--- init.sh
+++ init.sh
@@ -5,6 +5,8 @@ fdir=$(dirname "${BASH_SOURCE[0]}")
 function f() {
     if test "$#" -eq 1 && test -d "$1"; then
         cd "$1"
+    elif test "$#" -eq 1 && test "$1" == "-"; then
+        cd -
     else
         python3 "$fdir/f.py" "$@"
     fi
